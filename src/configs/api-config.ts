eee86ee94773bfacda24b1be5d6a87ef155d76b9
duynt2499 <118056890+duynt99@users.noreply.github.com>
2023-03-23 00:22:14 +0700

    update

--- src/configs/api-config.ts
+++ src/configs/api-config.ts
@@ -1,5 +1,5 @@
 const config = {
-  BASE_URL: import.meta.env.VITE_BASE_URL,
+  BASE_URL: "https://sandbox.101digital.io",
   client_id: import.meta.env.VITE_CLIENT_ID,
   client_secret: import.meta.env.VITE_CLIENT_SECRET,
 };
